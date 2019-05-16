a04938c66dc66ebf9af4c7b33e210dde0435ab92
Mac Strelioff <mstrelio@uci.edu>
2019-05-16 00:00:10 -0700

    auto

--- RockPaperScissorsAgent/app.R
+++ RockPaperScissorsAgent/app.R
@@ -11,7 +11,7 @@
 # macstrelioff.shinyapps.io
 # to host an app, run:
 # rsconnect::deployApp("<PATH TO APP>")
-# rsconnect::deployApp("/Users/mac/git/macstrelioff.github.io/MacStrelioff_Source/content/Unlisted/Example")
+# rsconnect::deployApp("/Users/mac/git/RockPaperScissors/RockPaperScissorsAgent")
 # URL on shinyapps.io will be folder name of this project, the last part of the path
 
 # for a rock-paper-scissors bot, can enumerate 5-gram outcomes with; 
@@ -26,23 +26,39 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   # Application title
-   titlePanel("Rock Paper Scissors Bot"),
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         actionButton("rock",
-                      "Rock"),
-         actionButton("paper",
-                      "Paper"),
-         actionButton("scissors",
-                      "Scissors")
-         ),
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot") # breaks for a name other than distPlot?
-      )
-   )
+  # Application title
+  titlePanel("Rock Paper Scissors!"),
+  # figure
+  fluidRow(
+    column(width=5,
+           plotOutput("distPlot")
+    )
+  ),
+  # buttons
+  fluidRow(
+    column(width=5,offest=2,
+           actionButton("rock","Rock"),
+           actionButton("paper","Paper"),
+           actionButton("scissors","Scissors")
+    )
+  ),
+  fluidRow(width=5,offset=5,
+           textOutput("result"),br(),
+           p("Source code available at: https://github.com/MacStrelioff/RockPaperScissors")
+           )
+  
+   #sidebarLayout(
+   #   sidebarPanel(position="right",
+   #      position="right",
+   #      actionButton("rock","Rock"),
+   #      actionButton("paper","Paper"),
+   #      actionButton("scissors","Scissors")
+   #      ),
+   #   # Show a plot of the generated distribution
+   #   mainPanel(
+   #     plotOutput("distPlot") # breaks for a name other than distPlot?
+   #   )
+   #)
 )
 
 # Define server logic required to draw a histogram
@@ -55,6 +71,7 @@ server <- function(input, output) {
   values$score =0; # track score
   values$scores=0; # track score history for feedback
   values$grams = data.frame('rrrrr'=rep(0,3)) # initialize to store gram counts
+  values$a = "init";
   values$as = c("r","p","s") # possible actions
 
   # text feedback
@@ -80,9 +97,8 @@ server <- function(input, output) {
             values$grams[ngram]=rep(0,3)
             values$a=sample(values$as,1)
           } else { # if at least 5 actions taken, and this pattern has been seen before, 
-            values$a=values$as[which.min(values$grams[ngram][[1]])]
-            # FIX LOGIC, 
-            # currently, pick the action that opponent chooses the least
+            pred = values$as[which.max(values$grams[ngram][[1]])]
+            values$a=switch(pred,"r"="p","p"="s","s"="r")
           }
           cat("\n",names(values$grams))
           cat("\n",values$grams[ngram][[1]])
@@ -91,15 +107,15 @@ server <- function(input, output) {
         # get opponent action and outcome
         if(input$rock    -sum(values$opp_actions=="r")==1){
           opp_action="r"
-          dscore = switch(values$a,"r"=0,"p"=1,"s"=-1)
+          dscore = switch(values$a,"r"=0,"p"=-1,"s"=1)
           }
         if(input$paper   -sum(values$opp_actions=="p")==1){
           opp_action="p"
-          dscore = switch(values$a,"r"=-1,"p"=0,"s"=1)
+          dscore = switch(values$a,"r"=1,"p"=0,"s"=-1)
         }
         if(input$scissors-sum(values$opp_actions=="s")==1){
           opp_action="s"
-          dscore = switch(values$a,"r"=1,"p"=-1,"s"=0)
+          dscore = switch(values$a,"r"=-1,"p"=1,"s"=0)
         }
         
         # evaluate outcome
@@ -109,11 +125,11 @@ server <- function(input, output) {
         # update opponent model 
         values$opp_actions = c(values$opp_actions,opp_action);
         
-        #if(length(values$opp_actions)>5){
-        #  if(any(names(values$grams)==ngram)){
-        #    values$grams[ngram]=values$grams[ngram]+values$as==opp_action
-        #  }
-        #}
+        if(length(values$opp_actions)>5){
+          if(any(names(values$grams)==ngram)){
+            values$grams[ngram][[1]]=values$grams[ngram][[1]]+(values$as==opp_action)
+          }
+        }
         
       }
     
@@ -123,12 +139,17 @@ server <- function(input, output) {
       x = seq(0,values$round);
       y = values$scores;
       cat("\n round:",values$round, ", score:",values$score,", len(x): ",length(x)," len(y):",length(y),", opp_act:",values$opp_actions,
+          "\n a: ",values$a,
           sep="")
       # draw the histogram with the specified number of bins
-      plot(x,y,type="l",xlab = "Rounds",ylab="Score")
+      plot(x,y,type="l",xlab = "Rounds",ylab="Score",main="Cumulative Score")
       })
     })
     })
+  
+  output$result = renderText({
+    paste("Opponent chose: ",switch(values$a,"r"="Rock","p"="Paper","s"="Scissors","init"="Nothing yet, ..."))
+  })
   
 }
 
